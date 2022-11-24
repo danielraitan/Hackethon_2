376eff9b0e1e02946dde6b2ea4a701334573b007
daniel <raitandaniel@gmail.com>
2022-11-24 21:05:33 +0200

    hackethon

---
+++ hackethon_2.sql
@@ -0,0 +1,16 @@
+-- create table football(
+-- id serial primary key,
+-- player_name VARCHAR(25),
+-- date_of_birth date,
+-- country_of_birth VARCHAR(25),
+-- team varchar(25),
+-- matches_played int,
+-- goals int,
+-- trophies int
+-- );
+
+-- insert into football(player_name, date_of_birth, country_of_birth, team,matches_played, goals,trophies)VALUES
+-- ('cristiano ronaldo', '1985-02-05', 'portugal', 'manchester united', 1139, 818, 32),
+-- ('lionel messi', '1987-06-24', 'argentina', 'psg', 778, 672, 35),
+-- ('neymar jr', '1992-02-05', 'brazil', 'psg', 695, 430, 28),
+-- ('karim benzema', '1987,12,19', 'france', 'real madrid', 862, 432, 23);
